a0232d8c966ab8a2ef090d6d705b6d6857149bd3
Coons, Mitchell R <mrcoons@iu.edu>
2024-04-17 22:23:14 -0400

    Added reverse state

--- GameTechFinal/Assets/Scripts/Player.cs
+++ GameTechFinal/Assets/Scripts/Player.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+   public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    public float jump;
 
+    private bool reverseState;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        reverseState = false;
+    }
     void Update()
     {
-        // Check if the D key is pressed
         if (Input.GetKey(KeyCode.D))
         {
             // Move right
@@ -18,5 +25,22 @@ public class PlayerMovement : MonoBehaviour
             // Move left
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
         }
+        if (Input.GetKeyDown(KeyCode.Space) && !reverseState){
+            transform.position = new Vector2(transform.position.x, transform.position.y - 4f);
+            rb.gravityScale = -3;
+            reverseState = true;
+        } else if (Input.GetKeyDown(KeyCode.Space) && reverseState == true){
+            transform.position = new Vector2(transform.position.x, transform.position.y + 4f);
+            rb.gravityScale = 3;
+            reverseState = false;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !reverseState)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+            }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && reverseState)
+    {
+    rb.AddForce(new Vector2(rb.velocity.x, -jump));
+}
     }
 }
