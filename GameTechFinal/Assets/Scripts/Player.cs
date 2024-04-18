3956677605a0119791233a1694c3568daf05752c
Coons, Mitchell R <mrcoons@iu.edu>
2024-04-18 13:43:05 -0400

    Animations pushed

--- GameTechFinal/Assets/Scripts/Player.cs
+++ GameTechFinal/Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour
    public float moveSpeed = 5f;
     private Rigidbody2D rb;
     public float jump;
+    private Animator m_Animator;
+    private bool jumping;
 
     private bool reverseState;
     float jumpCooldown = 1.0f;
@@ -12,21 +14,24 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        m_Animator = gameObject.GetComponent<Animator>();
         reverseState = false;
+        jumping = false;
     }
     void Update()
     {
         timeSinceJump += Time.deltaTime;
         if (Input.GetKey(KeyCode.D))
         {
-            // Move right
             transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            m_Animator.SetBool("walking", true);
         }
         // Check if the A key is pressed
         else if (Input.GetKey(KeyCode.A))
         {
             // Move left
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            m_Animator.SetBool("walking", true);
         }
         if (Input.GetKeyDown(KeyCode.Space) && !reverseState){
             transform.position = new Vector2(transform.position.x, transform.position.y - 4f);
@@ -47,5 +52,14 @@ public class Player : MonoBehaviour
                 timeSinceJump = 0;
                 rb.AddForce(new Vector2(rb.velocity.x, -jump));
             }
+        if (timeSinceJump <= jumpCooldown){
+            m_Animator.SetBool("jumping", true);
+        } else {
+            m_Animator.SetBool("jumping", false);
+            jumping = false;
+        }
+        if (!Input.anyKey && !jumping){
+            m_Animator.SetBool("walking", false);
+        }
     }
 }
